0fe83e26b0e703735352c8d6755730ab8b645ef2
Larry Tenny <ltenny@gmail.com>
2025-05-02 14:24:22 -0500

    fix

--- Section-3/final/src/App.jsx
+++ Section-3/final/src/App.jsx
@@ -3,8 +3,9 @@ function App() {
   const msg2 = "This is a message from mars";
   return (
     <>
+      <h1>Header goes here</h1>
       <div>{msg1}</div>
-      <div>{msg2}</div>
+      <h2>{msg2}</h2>
     </>
   );
 }
