524ef1eea7c282c93ca4489aa16c9e15e648fe5f
Larry Tenny <ltenny@gmail.com>
2025-05-06 14:13:59 -0500

    initial commit

--- Lecture_09/canary/src/App.jsx
+++ Lecture_09/canary/src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="mx-6 text-2xl text-blue-600">Hello world!</div>;
+  return <div className="mx-3.5 text-2xl text-blue-500">Hello world!</div>;
 }
 
 export default App;
