cf6f10c4d1a119d6fb16cfb893ddd06778c287a1
Larry Tenny <ltenny@gmail.com>
2025-05-13 08:05:36 -0500

    initial commit

--- Lecture_12/canary/src/App.jsx
+++ Lecture_12/canary/src/App.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Main from "./components/Main";
+import About from "./components/About";
+import Login from "./components/Login";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  return <div className="mx-3.5 text-2xl text-blue-500">Hello world!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate replace to="app" />} />
+        <Route path="/" element={<HomePage />}>
+          <Route index element={<Navigate replace to="app" />} />
+          <Route path="app" element={<Main />} />
+          <Route path="about" element={<About />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
